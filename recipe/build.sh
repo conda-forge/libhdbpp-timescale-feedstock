5cb11b049f625891619a49fd27b328f35a7927da
Johan Forsberg <johan.forsberg@maxiv.lu.se>
2023-01-17 16:27:22 +0100

    Add separate package with debug symbols.

--- recipe/build.sh
+++ recipe/build.sh
@@ -4,3 +4,9 @@ cmake ${CMAKE_ARGS} \
 
 cmake --build build -j $CPU_COUNT
 cmake --build build --target install
+
+# Separate debugging symbols
+${OBJCOPY} --only-keep-debug ${PREFIX}/lib/libhdb++timescale.so.${PKG_VERSION} ${PREFIX}/lib/libhdb++timescale.so.${PKG_VERSION}.dbg
+chmod 664 ${PREFIX}/lib/libhdb++timescale.so.${PKG_VERSION}.dbg
+${OBJCOPY} --strip-debug ${PREFIX}/lib/libhdb++timescale.so.${PKG_VERSION}
+${OBJCOPY} --add-gnu-debuglink=${PREFIX}/lib/libhdb++timescale.so.${PKG_VERSION}.dbg ${PREFIX}/lib/libhdb++timescale.so.${PKG_VERSION}
