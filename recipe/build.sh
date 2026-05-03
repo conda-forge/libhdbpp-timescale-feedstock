a2fc631eaec818eeea59ed836281e97a7ec3499b
Benjamin Bertrand <benjamin.bertrand@maxiv.lu.se>
2026-05-03 15:56:52 +0200

    Migrate recipe to v1 format

--- recipe/build.sh
+++ recipe/build.sh
@@ -10,8 +10,8 @@ cmake --build build --target install
 # Separate debugging symbols on Linux
 if [ -n "${OBJCOPY}" ]
 then
-  ${OBJCOPY} --only-keep-debug ${PREFIX}/lib/libhdb++timescale.so.${PKG_VERSION} ${PREFIX}/lib/libhdb++timescale.so.${PKG_VERSION}.dbg
-  chmod 664 ${PREFIX}/lib/libhdb++timescale.so.${PKG_VERSION}.dbg
-  ${OBJCOPY} --strip-debug ${PREFIX}/lib/libhdb++timescale.so.${PKG_VERSION}
-  ${OBJCOPY} --add-gnu-debuglink=${PREFIX}/lib/libhdb++timescale.so.${PKG_VERSION}.dbg ${PREFIX}/lib/libhdb++timescale.so.${PKG_VERSION}
+  ${OBJCOPY} --only-keep-debug ${PREFIX}/lib/libhdb++timescale.so.${LIBHDBPP_TIMESCALE_VERSION} ${PREFIX}/lib/libhdb++timescale.so.${LIBHDBPP_TIMESCALE_VERSION}.dbg
+  chmod 664 ${PREFIX}/lib/libhdb++timescale.so.${LIBHDBPP_TIMESCALE_VERSION}.dbg
+  ${OBJCOPY} --strip-debug ${PREFIX}/lib/libhdb++timescale.so.${LIBHDBPP_TIMESCALE_VERSION}
+  ${OBJCOPY} --add-gnu-debuglink=${PREFIX}/lib/libhdb++timescale.so.${LIBHDBPP_TIMESCALE_VERSION}.dbg ${PREFIX}/lib/libhdb++timescale.so.${LIBHDBPP_TIMESCALE_VERSION}
 fi
